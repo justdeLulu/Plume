99ba15da2518f28b79d388aafc0eae6e1fdb592a
deLulu <lucasfert75@gmail.com>
2025-02-22 20:05:45 +0100

    feat(config): add config

---
+++ config.py
@@ -0,0 +1,14 @@
+# Your testing guild IDs
+GUILD_IDS = [444]
+
+# The cogs you don't want to load
+LOAD_EXCEPTIONS = []
+
+# The directory where your cogs are located
+COGS_DIR = "cogs"
+
+# Whether or not the bot should automatically reload cogs when a change is made
+AUTO_RELOAD = True
+
+#Subject
+SUBJECT_ID = 444
